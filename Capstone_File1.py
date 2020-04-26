12483bca7d6ecd85d9111018e1c46041822eb999
tanayamodak <48873151+tanayamodak@users.noreply.github.com>
2020-04-26 11:25:30 +0530

    Add files via upload

---
+++ Capstone_File1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook is a part of capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course')
+
+
+# In[ ]:
+
+
+
+
